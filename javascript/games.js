2923b304c150a68dab22741bcd8042fa4d85436c
Michael <mduron432@gmail.com>
2023-12-02 00:08:51 -0600

    clicking buy now, now generates a modal whgere you can choose numbers or random numbers. currently only prints to console when you buy

--- javascript/games.js
+++ javascript/games.js
@@ -1,18 +1,38 @@
 const gameData = [
     {
-        game: 'Powerball', price:"$15", jackpot:"$374,000,000", drawDate:"12/1/2023", imageSRC:"https://i.imgur.com/xEpAlA5.png"
+        game: 'Powerball', 
+        price: "$15", 
+        jackpot: "$374,000,000", 
+        drawDate: "12/1/2023", 
+        imageSRC: "https://i.imgur.com/xEpAlA5.png",
+        numberOfBalls: 5
     },
     {
-        game: 'Mega Millions', price:"$13", jackpot:"$355,000,000", drawDate:"12/1/2023", imageSRC:"https://i.imgur.com/BTrhEMe.jpg"
-
+        game: 'Mega Millions', 
+        price: "$13", 
+        jackpot: "$355,000,000", 
+        drawDate: "12/1/2023", 
+        imageSRC: "https://i.imgur.com/BTrhEMe.jpg",
+        numberOfBalls: 5
     },
     {
-        game: 'Lotto Texas', price:"$8", jackpot:"$25,750,000", drawDate:"12/1/2023", imageSRC:"https://i.imgur.com/UZrBt1v.png"
+        game: 'Lotto Texas', 
+        price: "$8", 
+        jackpot: "$25,750,000", 
+        drawDate: "12/1/2023", 
+        imageSRC: "https://i.imgur.com/UZrBt1v.png",
+        numberOfBalls: 6
     },
     {
-        game: 'Texas Two Step', price:"$5", jackpot:"$200,000", drawDate:"12/1/2023", imageSRC:"https://i.imgur.com/O1KF6TG.png"
+        game: 'Texas Two Step', 
+        price: "$5", 
+        jackpot: "$200,000", 
+        drawDate: "12/1/2023", 
+        imageSRC: "https://i.imgur.com/O1KF6TG.png",
+        numberOfBalls: 4
     }
-]
+];
+
 document.querySelector('.logo').addEventListener('click', function() {
     window.location.href = '../index.html';
 });
@@ -124,9 +144,92 @@ function createTableRows() {
 
 // Function to handle the button click event
 function buyNowClicked(gameName) {
-    console.log(gameName + ' Buy Now clicked');
-    // Handle the click event (e.g., navigate to a purchase page)
+    const selectedGame = gameData.find((game) => game.game === gameName);
+
+    // Check if a game with the specified name was found
+    if (selectedGame) {
+        createModal(selectedGame);
+    } else {
+        console.error(`Game data not found for '${gameName}'`);
+    }
 }
 
 // Call the function to create the rows when the page loads
+document.addEventListener('DOMContentLoaded', createTableRows);
+
+
+// Function to generate the input fields based on the number of balls
+function generateInputFields(numBalls) {
+    let inputFields = '';
+    for (let i = 0; i < numBalls; i++) {
+        inputFields += `<input class="ball" maxlength="2">`;
+    }
+    console.log(inputFields)
+    return inputFields;
+}
+// Function to create and display the modal
+function createModal(gameData) {
+    // Create a modal element
+    const modal = document.createElement('div');
+    modal.id = 'modal';
+
+    // Construct the modal content based on the game data
+    modal.innerHTML = `
+        <button id="exit">X</button>
+        <h1 id="modalTitle">${gameData.game}</h1>
+        <h2 id="modalJackpot">Jackpot: ${gameData.jackpot}</h2>
+        <h2 id="modalPrice">Price: ${gameData.price}</h2>
+        <form id="ballArea">
+            ${generateInputFields(gameData.numberOfBalls)}
+        </form>
+        <form>
+            <input type="checkbox" id="choose">Choose for me
+        </form>
+        <button id="modalButton">Buy</button>
+    `;
+
+    // Append the modal to the body
+    document.body.appendChild(modal);
+
+
+    // Event listener to close the modal when the "X" button is clicked
+    const exitButton = document.getElementById('exit');
+    exitButton.addEventListener('click', () => {
+        document.body.removeChild(modal);
+    });
+
+    // Event listener to handle the "Choose for me" checkbox
+    const chooseCheckbox = document.getElementById('choose');
+    chooseCheckbox.addEventListener('change', () => {
+        const inputFields = document.querySelectorAll('.ball');
+        if (chooseCheckbox.checked) {
+            inputFields.forEach((input) => {
+                input.value = getRandomNumber(0, 99);
+            });
+        } else {
+            inputFields.forEach((input) => {
+                input.value = '';
+            });
+        }
+    });
+
+    // Event listener to handle the "Buy" button click
+    const buyButton = document.getElementById('modalButton');
+    buyButton.addEventListener('click', () => {
+        const inputFields = document.querySelectorAll('.ball');
+        const selectedNumbers = Array.from(inputFields).map((input) => input.value);
+        console.log('Selected Numbers:', selectedNumbers);
+        document.body.removeChild(modal);
+    });
+}
+
+
+// Function to get a random number between min and max (inclusive)
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+// Call the function to create the table rows when the page loads
 document.addEventListener('DOMContentLoaded', createTableRows);
